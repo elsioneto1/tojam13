704d3c0addf90587e8e5cedd7dd90b6f74dc092f
elsio <elsioneto@gmail.com>
2018-05-04 20:38:39 -0400

    FIrst iteration scene and placeholders

---
+++ Assets/Scripts/Utilities/MovementObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class MovementObject : ScriptableObject {
+
+
+    public float test;
+
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/MovementObject")]
+    public static void CreateAsset()
+    {
+        ScriptableObjectUtility.CreateAsset<MovementObject>();
+    }
+#endif
+
+
+}
