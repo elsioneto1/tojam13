17d6c91f70001fb0fd18c40892df0e089254b56d
Fodrigo Rorti <rodrigomforti@gmail.com>
2018-05-05 18:26:20 -0400

    fixed bugs

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour {
+
+	private bool isCounting = false;
+	int currentTime;
+
+	TextMesh tmComponent;
+
+	private void Awake()
+	{
+		tmComponent = GetComponent<TextMesh>();
+	}
+
+	public void SetTime(int time)
+	{
+		isCounting = true;
+		currentTime = time;
+		tmComponent.text = currentTime.ToString();
+		StartCoroutine(CountTime());
+	}
+
+	private IEnumerator CountTime()
+	{
+		while(currentTime > 0)
+		{
+			yield return new WaitForSeconds(1);
+			currentTime--;
+			tmComponent.text = currentTime.ToString();
+		}
+
+		isCounting = false;
+		tmComponent.text = string.Empty;
+	}
+
+}
