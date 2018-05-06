8563837b412e44e3ed71c5ba348dd87f74408d0c
Rodrigo Forti <rodrigomforti@gmail.com>
2018-05-06 17:49:34 -0400

    Implemented art

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public string sceneToLoad; 
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button7))
+                SceneManager.LoadScene(sceneToLoad);
+    }
+}
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour {
+
+    SpriteRenderer sr;
+    int alphaDecay = 5;
+    int alpha = 255;
+    // Use this for initialization
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (this.transform.position.y < 20)
+            Destroy(this);
+
+        alpha -= alphaDecay;
+
+    //    sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, (float)alpha / 255f);
+	}
+}
